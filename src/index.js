c1d8f78def75931d490d10b9796839c868ecdd41
andrey-jey <kozlov13.an@mail.ru>
2020-03-19 15:52:17 +0300

    done-1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+exports.min = function min (array) {
+    if (array == undefined) return 0;
+    if (array.length > 0) return Math.min(...array) 
+    else return 0;
+  }
+  
+  exports.max = function max (array) {
+    if (array == undefined) return 0;
+    if (array.length > 0) return Math.max(...array)
+    else return 0;
+  }
+  
+  exports.avg = function avg (array) {
+    if (array == undefined) return 0;
+    if (array.length > 0) return array.reduce((acc, item) => acc + item) / array.length; 
+    else return 0;
+  }
